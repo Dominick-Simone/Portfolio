32e448759bc39e1324e246e898e93b0d74e269c9
Dominick Simone <domsimone123@gmail.com>
2024-01-10 22:57:10 -0500

    Updating Resume src

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-import Resume from '../../Resume.pdf'
+import Resume from './Resume.pdf'
 const Resume = () => {
   return (
     <>
