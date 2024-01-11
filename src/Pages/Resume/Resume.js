9d1ac48f7459e315351c0e22bccf2adac18251fd
Dominick Simone <domsimone123@gmail.com>
2024-01-10 22:59:20 -0500

    Updating Resume src

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-import Resume from './Resume.pdf'
+import ResumePDF from './Resume.pdf'
 const Resume = () => {
   return (
     <>
     <Header />
     <div className='flex justify-center bg-gray-300'>
 
-    <embed src={Resume} className='h-screen w-4/5' />
+    <embed src={ResumePDF} className='h-screen w-4/5' />
     </div>
     <Footer />
     </>
