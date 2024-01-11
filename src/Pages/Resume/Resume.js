6798af815bd462bfaa70bc09b9d0928a2cebf03e
Dominick Simone <domsimone123@gmail.com>
2024-01-10 23:04:32 -0500

    Updating Resume src

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -8,7 +8,7 @@ const Resume = () => {
     <Header />
     <div className='flex justify-center bg-gray-300'>
 
-    <embed src={ResumePDF} className='h-screen w-4/5' />
+    <embed src={'https://drive.google.com/file/d/1gJwHPprook4xDFypbE1snynr7g7hPKHd/view?usp=sharing'} className='h-screen w-4/5' />
     </div>
     <Footer />
     </>
