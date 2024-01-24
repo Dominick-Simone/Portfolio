0ba0c462f161118e808be7be260c447e6122807d
Dominick Simone <domsimone123@gmail.com>
2024-01-24 12:39:41 -0500

    updating about sizing

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-import ResumePDF from './Resume.pdf'
 const Resume = () => {
   return (
     <>
